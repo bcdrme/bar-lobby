cc70972242008bccfe0e8156e953a4c741e3f8bb
Benoit Coudour <benoit.coudour@gmail.com>
2024-10-14 19:04:01 +0200

    fix some scss warning

--- src/renderer/views/debug/controls.vue
+++ src/renderer/views/debug/controls.vue
@@ -3,85 +3,92 @@
 </route>
 
 <template>
-    <div class="controls flex-col gap-md">
-        <div class="flex-row gap-md flex-center-items">
-            <Button> Button </Button>
-            <Button :disabled="true"> Disabled Button </Button>
-            <Button v-tooltip="'Red button'" class="red"> Red Button </Button>
-            <Button v-tooltip.bottom="'Blue button'" class="blue"> Blue Button </Button>
-        </div>
-        <div class="flex-row gap-md flex-center-items">
-            <Textbox :modelValue="text" @update:model-value="onUpdateText" />
-            <Textbox v-model="text" label="Label" />
-            <Textbox v-model="text" :disabled="true" />
-            <div class="value">{{ text }}</div>
-        </div>
-        <div class="flex-row gap-md flex-center-items">
-            <Number :modelValue="num" showButtons @update:model-value="onUpdateNum" />
-            <Number v-model="num" label="Label" showButtons />
-            <Number v-model="num" :disabled="true" />
-            <div class="value">{{ num }}</div>
-        </div>
-        <div class="flex-row gap-md flex-center-items">
-            <Select
-                :modelValue="selection"
-                :options="selections"
-                optionLabel="name"
-                optionValue="value"
-                :placeholder="selection"
-                @update:model-value="onUpdateSelection"
-            />
-            <Select
-                v-model="selection"
-                :options="selections"
-                label="Label"
-                optionLabel="name"
-                optionValue="value"
-                :placeholder="selection"
-                :filter="true"
-            />
-            <Select
-                v-model="selection"
-                :options="selections"
-                optionLabel="name"
-                optionValue="value"
-                :placeholder="selection"
-                :disabled="true"
-            />
-            <div class="value">{{ selection }}</div>
-        </div>
-        <div class="flex-row gap-md flex-center-items">
-            <Range :modelValue="range" @update:model-value="onUpdateRange" />
-            <Range v-model="range" label="Label" />
-            <Range v-model="range" :disabled="true" />
-            <div class="value">{{ range }}</div>
-        </div>
-        <div class="flex-row gap-md flex-center-items">
-            <Checkbox :modelValue="checked" @update:model-value="onUpdateChecked" />
-            <Checkbox v-model="checked" label="Label" />
-            <Checkbox v-model="checked" :disabled="true" />
-            <div class="value">{{ checked }}</div>
-        </div>
-        <div class="flex-row gap-md flex-center-items">
-            <Options :modelValue="option" :options="options" @update:model-value="onUpdateOption" />
-            <Options v-model="option" :options="options" label="Label" />
-            <Options v-model="option" :options="options" :unselectable="false" />
-            <Options v-model="option" :options="options" :disabled="true" />
-        </div>
-        <div class="flex-row">
-            <Textbox v-model="text" />
-            <Select
-                v-model="selection"
-                :options="selections"
-                optionLabel="name"
-                optionValue="value"
-                :placeholder="selection"
-                :filter="true"
-            />
-            <Range v-model="range" />
-            <Checkbox v-model="checked" />
-            <Options v-model="option" :options="options" />
-        </div>
+    <div class="view">
+        <Panel class="flex-grow">
+            <div class="controls flex-col gap-md">
+                <div class="flex-row gap-md flex-center-items">
+                    <Button> Button </Button>
+                    <Button :disabled="true"> Disabled Button </Button>
+                    <Button v-tooltip="'Red button'" class="red"> Red Button </Button>
+                    <Button v-tooltip.bottom="'Blue button'" class="blue"> Blue Button </Button>
+                </div>
+                <div class="flex-row gap-md flex-center-items">
+                    <Textbox :modelValue="text" @update:model-value="onUpdateText" />
+                    <Textbox v-model="text" label="Label" />
+                    <Textbox v-model="text" :disabled="true" />
+                    <div class="value">{{ text }}</div>
+                </div>
+                <div class="flex-row gap-md flex-center-items">
+                    <SearchBox v-model="text" />
+                </div>
+                <div class="flex-row gap-md flex-center-items">
+                    <Number :modelValue="num" showButtons @update:model-value="onUpdateNum" />
+                    <Number v-model="num" label="Label" showButtons />
+                    <Number v-model="num" :disabled="true" />
+                    <div class="value">{{ num }}</div>
+                </div>
+                <div class="flex-row gap-md flex-center-items">
+                    <Select
+                        :modelValue="selection"
+                        :options="selections"
+                        optionLabel="name"
+                        optionValue="value"
+                        :placeholder="selection"
+                        @update:model-value="onUpdateSelection"
+                    />
+                    <Select
+                        v-model="selection"
+                        :options="selections"
+                        label="Label"
+                        optionLabel="name"
+                        optionValue="value"
+                        :placeholder="selection"
+                        :filter="true"
+                    />
+                    <Select
+                        v-model="selection"
+                        :options="selections"
+                        optionLabel="name"
+                        optionValue="value"
+                        :placeholder="selection"
+                        :disabled="true"
+                    />
+                    <div class="value">{{ selection }}</div>
+                </div>
+                <div class="flex-row gap-md flex-center-items">
+                    <Range :modelValue="range" @update:model-value="onUpdateRange" />
+                    <Range v-model="range" label="Label" />
+                    <Range v-model="range" :disabled="true" />
+                    <div class="value">{{ range }}</div>
+                </div>
+                <div class="flex-row gap-md flex-center-items">
+                    <Checkbox :modelValue="checked" @update:model-value="onUpdateChecked" />
+                    <Checkbox v-model="checked" label="Label" />
+                    <Checkbox v-model="checked" :disabled="true" />
+                    <div class="value">{{ checked }}</div>
+                </div>
+                <div class="flex-row gap-md flex-center-items">
+                    <Options :modelValue="option" :options="options" @update:model-value="onUpdateOption" />
+                    <Options v-model="option" :options="options" label="Label" />
+                    <Options v-model="option" :options="options" :unselectable="false" />
+                    <Options v-model="option" :options="options" :disabled="true" />
+                </div>
+                <div class="flex-row">
+                    <Textbox v-model="text" />
+                    <Select
+                        v-model="selection"
+                        :options="selections"
+                        optionLabel="name"
+                        optionValue="value"
+                        :placeholder="selection"
+                        :filter="true"
+                    />
+                    <Range v-model="range" />
+                    <Checkbox v-model="checked" />
+                    <Options v-model="option" :options="options" />
+                </div>
+            </div>
+        </Panel>
     </div>
 </template>
 
@@ -95,6 +102,8 @@ import Options from "@renderer/components/controls/Options.vue";
 import Range from "@renderer/components/controls/Range.vue";
 import Select from "@renderer/components/controls/Select.vue";
 import Textbox from "@renderer/components/controls/Textbox.vue";
+import Panel from "@renderer/components/common/Panel.vue";
+import SearchBox from "@renderer/components/controls/SearchBox.vue";
 
 const text = ref("textbox");
 function onUpdateText(newText: string) {
